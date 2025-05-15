7f04c2e6a8ecdbba60acb9668a4c648fb12db62f
DanielMauderer <38982952+DanielMauderer@users.noreply.github.com>
2025-05-16 01:09:49 +0200

    Grn 62 refactor docker files (#22)
    * add new workflow
    
    * change to ubuntu
    
    * add file destination
    
    * remove arm for testing
    
    * change to nightly
    
    * fix version
    
    * more steps
    
    * correct tags
    
    * more generic target
    
    * changes
    
    * pipeline changes
    
    * keep alive
    
    * update docker file
    
    * change to kaniko
    
    * add dockerfile path
    
    * remove tag for now
    
    * hard coded tag
    
    * dyn tags
    
    * add permission
    
    * test arm
    
    * add arm target
    
    * target
    
    * add second runner
    
    * different version
    
    * only arm
    
    * try different flow
    
    * rub pipeline
    
    * pipeline
    
    * run pipeline
    
    * change tag
    
    * lolwercase
    
    * flow
    
    * flow
    
    * clean up pipeline
    
    * clean up cargo toml
    
    * clean up old ci
    
    * undo changes
    
    * change Docker Files
    
    * update docker file
    
    * fix hado
    
    * update pipeline
    
    * update script
    
    * edit dockerfile
    
    * fix cp
    
    * update copy
    
    * script
    
    * .
    
    * test publish
    
    * change text
    
    * change remaining docker files
    
    * add missing service builds to workflow
    
    * copy files to correct location
    
    * fix docker path
    
    * add correct trigger
    
    * pipeline trigger
    
    * remove from config

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+ARG RUST_VERSION=1.86.0
+
+FROM rust:${RUST_VERSION}-slim-bookworm AS builder
+WORKDIR /app
+COPY . .
+
+RUN apt-get update -y && apt-get upgrade -y && \ 
+  apt-get install --no-install-recommends -y \
+    pkg-config=1.8.1-1 \
+    libssl-dev=3.0.15-1~deb12u1 \
+    libpq-dev=15.12-0+deb12u2  && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+
+COPY scripts/build-image-layer.sh /tmp/
+RUN sh /tmp/build-image-layer.sh tools
+
+# Build the application
+RUN sh /tmp/build-image-layer.sh apps && \
+  find "./target/release/" -maxdepth 1 -type f -exec test -x {} \; -exec cp {} / \;
---
+++ scripts/build-image-layer.sh
@@ -0,0 +1,49 @@
+if [ -z "$TARGETARCH" ]; then
+  :
+else
+  case "${TARGETARCH}" in
+    "amd64")
+      LINKER_NAME="x86_64-linux-gnu-gcc"
+      LINKER_PACKAGE="gcc-x86-64-linux-gnu"
+      BUILD_TARGET="x86_64-unknown-linux-gnu" ;;
+    "arm64")
+      LINKER_NAME="aarch64-linux-gnu-gcc"
+      LINKER_PACKAGE="gcc-aarch64-linux-gnu"
+      BUILD_TARGET="aarch64-unknown-linux-gnu" ;;
+  esac
+fi
+
+tools() {
+  if [ -z "$TARGETARCH" ]; then
+    :
+  else
+    apt-get install -y "${LINKER_PACKAGE}"
+    rustup target add "${BUILD_TARGET}"
+  fi
+}
+
+deps() {
+  if [ -z "$TARGETARCH" ]; then
+    cargo build -j 10 --release
+  else
+    cargo build -j 10 --release --target "${BUILD_TARGET}"
+  fi
+}
+
+apps() {
+  if [ -z "$TARGETARCH" ]; then
+    cargo build -j 10 --release
+  else
+    cargo build -j 10 --release --target "${BUILD_TARGET}"
+  fi
+}
+
+if [ -z "$TARGETARCH" ]; then
+  :
+else
+  export RUSTFLAGS="-C linker=${LINKER_NAME}"
+  export PKG_CONFIG_ALLOW_CROSS="1"
+  export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig"
+fi
+
+"$@"
