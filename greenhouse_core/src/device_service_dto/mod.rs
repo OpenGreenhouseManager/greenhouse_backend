a511a58fd23062c4636a0d4df203adc4ba48e4d7
Daniel Mauderer <daniel.mauderer@outlook.com>
2025-06-08 13:09:24 +0200

    create dtos

---
+++ greenhouse_core/src/device_service_dto/mod.rs
@@ -0,0 +1,3 @@
+pub mod endpoints;
+pub mod post_device;
+pub mod put_device;
--- services/device_service/src/router/device_router.rs
+++ services/device_service/src/router/device_router.rs
@@ -1,7 +1,62 @@
-use axum::Router;
-
+use super::error::Result;
 use crate::AppState;
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    response::IntoResponse,
+    routing::{get, post, put},
+};
+use greenhouse_core::device_service_dto::{
+    endpoints::CONFIG, post_device::PostDeviceDtoRequest, put_device::PutDeviceDtoRequest,
+};
+use uuid::Uuid;
 
 pub(crate) fn routes(state: AppState) -> Router {
-    Router::new().with_state(state)
+    Router::new()
+        .route("/", post(create_device))
+        .route("/", get(get_devices))
+        .route("/{id}", put(update_device))
+        .route("/{id}", get(get_device))
+        .route(&format!("/{{id}}/{CONFIG}"), get(get_device_config))
+        .with_state(state)
+}
+
+#[axum::debug_handler]
+pub(crate) async fn update_device(
+    State(AppState { config: _, pool }): State<AppState>,
+    Path(id): Path<Uuid>,
+    Json(update): Json<PutDeviceDtoRequest>,
+) -> Result<impl IntoResponse> {
+    Ok(Json(response))
+}
+
+#[axum::debug_handler]
+pub(crate) async fn create_device(
+    State(AppState { config: _, pool }): State<AppState>,
+    Json(entry): Json<PostDeviceDtoRequest>,
+) -> Result<impl IntoResponse> {
+    Ok(Json(response))
+}
+
+#[axum::debug_handler]
+pub(crate) async fn get_device(
+    State(AppState { config: _, pool }): State<AppState>,
+    Path(id): Path<Uuid>,
+) -> Result<impl IntoResponse> {
+    Ok(Json(response))
+}
+
+#[axum::debug_handler]
+pub(crate) async fn get_device_config(
+    State(AppState { config: _, pool }): State<AppState>,
+    Path(id): Path<Uuid>,
+) -> Result<impl IntoResponse> {
+    Ok(Json(response))
+}
+
+#[axum::debug_handler]
+pub(crate) async fn get_devices(
+    State(AppState { config: _, pool }): State<AppState>,
+) -> Result<impl IntoResponse> {
+    Ok(Json(response))
 }
