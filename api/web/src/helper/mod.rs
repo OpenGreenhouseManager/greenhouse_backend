969c735bc873cb57cf7af67d37b03193f663719b
Daniel Mauderer <daniel.mauderer@outlook.com>
2026-02-02 22:44:41 +0100

    fmt

--- api/web/src/helper/mod.rs
+++ api/web/src/helper/mod.rs
@@ -6,7 +6,9 @@ pub(crate) mod token {
     use greenhouse_core::auth_service_dto::user_token::UserToken;
 
     pub(crate) fn get_claims_dangerous(token: String) -> Result<UserToken> {
-        let claims = jsonwebtoken::dangerous::insecure_decode(token).map_err(|_| Error::InvalidToken)?.claims;
+        let claims = jsonwebtoken::dangerous::insecure_decode(token)
+            .map_err(|_| Error::InvalidToken)?
+            .claims;
         Ok(claims)
     }
 }
